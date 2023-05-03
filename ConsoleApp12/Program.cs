942ad166dd1658bf5f38ba083c62fc08975af9b9
NHOY <NHOY@2210DR000919>
2023-05-04 00:41:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int picturesInAlbum = 52;
+            int picturesInRow = 3;
+            int filledSerises = picturesInAlbum / picturesInRow;
+            int remainingPictures = picturesInAlbum % picturesInRow;
+            Console.WriteLine(filledSerises);
+            Console.WriteLine(remainingPictures);
+        }
+    }
+}
